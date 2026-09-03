51c418bea24f1c913b7b593f4fa76b3b7e0d64a4
felicitasprimus-glitch <felicitas.primus@gmail.com>
2026-09-03 22:51:19 +0200

    Update page.js

--- app/(app)/stoneware/page.js
+++ app/(app)/stoneware/page.js
@@ -3,7 +3,7 @@ export default function StonewarePage() {
     <div style={{ margin: "-1.5rem -1rem 0", height: "calc(100dvh - 100px)" }}>
       <iframe
         title="Stoneware & Pflege"
-        src="https://stonewareapp.vercel.app/"
+        src="/stoneware.html"
         style={{ width: "100%", height: "100%", border: 0, display: "block" }}
         allow="clipboard-write; camera"
       />
